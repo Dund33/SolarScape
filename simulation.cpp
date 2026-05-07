070e8b5473e53de872bdac624c7f44e2ab77271f
Luke <warior1112@gmail.com>
2026-05-07 20:43:27 +0200

    Avoided unnecessary copy

--- simulation.cpp
+++ simulation.cpp
@@ -43,7 +43,7 @@ int main()
         config.centralBodyIndex;
 
     std::vector<Body> bodies =
-        config.bodies;
+        std::move(config.bodies);
 
     const Real minimumDistance = DistanceAnalysis::minimumDistanceFromMovingPoint(
         bodies,
